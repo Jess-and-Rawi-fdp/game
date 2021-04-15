2232f87f4599a9d66ad565f8f3cfb47a16b8b52d
RaouiRahmeni <rahmeniraoui@gmail.com>
2021-04-15 11:38:50 +0100

    org the project

--- 3lw.js
+++
@@ -1,7 +0,0 @@
-// var words = ['age','cat','ask','bee','dog','bye','buy','day','die','cup','cow','egg','fox','gas','gun','guy','hip','jet','joy','lab','mom','mug','not','own','owl','pie','run','the','tea','you','wow']
-// function hint (array){
-// var randomWord = array[Math.floor(Math.random()*array.length)]
-// var randomLetter = randomWord[Math.floor(Math.random()*randomWord.length)] 
-// 	return 'a word has ' + randomLetter +' in the ' + (randomWord.indexOf(randomLetter) +1) + ' postion'
-// }
-// var music=document.getElementById('music')
